9cf77deb19059ca605c047c7a67d10454b91fa65
gamal <gamal@first>
2023-07-10 23:11:35 +0300

    add version 2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@
 function App() {
   return (
     <>
-      <h1 className="gaga">gamal</h1>
-      <h1>gamal</h1>
+      <h1 className="gaga"> gamal </h1>
+      <h2>gamal</h2>
+    <p>version 1 by devolper gamal mohamed taha</p>
     </>
   );
 }
