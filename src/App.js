ac3173e2ff4ef11ec7f97fe264a42284d20bc712
gamal <gamal@first>
2023-07-10 23:27:14 +0300

    qqqq

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
     <>
       <h1 className="gaga"> gamal </h1>
-      <h2>gamal</h2>
+      <h2>new bransh</h2>
     <p>version 1 by devolper gamal mohamed taha</p>
     </>
   );
